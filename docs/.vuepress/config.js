181250e74327c29623a331873ad577d30d2dfb81
zhangxiaogeng7 <1475407531@qq.com>
2020-12-21 11:11:36 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,21 +2,21 @@ module.exports = {
     title: '前端文档',
     base: '/blog/',
     description: '',
-    dest: 'public',
-    themeConfig: {
-        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
-        repo: 'https://github.com/zhangxiaogeng7/blog',        
-        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
-        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
-        repoLabel: '查看源码',       
-        // 假如文档不是放在仓库的根目录下：
-        docsDir: 'docs',
-        lastUpdated: '最后更新时间',
-        nav: [
-            { text: 'Home', link: '/' },
+    // dest: 'public',
+    // themeConfig: {
+    //     // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+    //     repo: 'https://github.com/zhangxiaogeng7/blog',        
+    //     // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+    //     // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+    //     repoLabel: '查看源码',       
+    //     // 假如文档不是放在仓库的根目录下：
+    //     docsDir: 'docs',
+    //     lastUpdated: '最后更新时间',
+    //     nav: [
+    //         { text: 'Home', link: '/' },
             
-        ]
-    }
+    //     ]
+    // }
    
         
 
